a092c71bd9e89b882785cb0eff4a91a95c745779
iulianpro <iulian.prodea@gmail.com>
2020-05-13 23:07:48 +0100

    Added logout successfully message

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect, reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 from home.views import index
 
 # Create your views here.
 
 def logout(request):
     auth.logout(request)
+    messages.success(request, "Successfully logged out")
     return redirect(reverse('index'))
