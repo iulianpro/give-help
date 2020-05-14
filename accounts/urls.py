83582ba2546f3849c15263a3a0136021d875b093
iulianpro <iulian.prodea@gmail.com>
2020-05-14 10:44:25 +0100

    Added login view and url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import logout
+from .views import logout, login
 
 
 urlpatterns = [
-    url(r"^logout/", logout, name="logout"),
+    url(r'^logout/', logout, name='logout'),
+    url(r'^login/', login, name='login')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import redirect, reverse
+from django.shortcuts import redirect, reverse, render
 from django.contrib import auth, messages
 from home.views import index
 
 # Create your views here.
 
+
 def logout(request):
     auth.logout(request)
     messages.success(request, "Successfully logged out")
     return redirect(reverse('index'))
+
+
+def login(request):
+    return render(request, 'login.html')
