ae3b87489bd37616a02f4f19c6f9ed893b014023
iulianpro <iulian.prodea@gmail.com>
2020-05-20 01:45:01 +0100

    Added basic password reset functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from accounts.views import logout, login, registration, view_profile, edit_profile
+from accounts import url_reset
 
 
 urlpatterns = [
@@ -7,5 +8,6 @@ urlpatterns = [
     url(r'^login/$', login, name='login'),
     url(r'^register/$', registration, name="registration"),
     url(r'^profile/$', view_profile, name="view_profile"),
-    url(r'^profile/edit/$', edit_profile, name="edit_profile")
+    url(r'^profile/edit/$', edit_profile, name="edit_profile"),
+    url(r'^password-reset/', include(url_reset))
 ]
--- give_help/settings.py
+++ give_help/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
