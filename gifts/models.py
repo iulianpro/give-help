ec379e3db95bb9b4c888dc91cff1874bc8615c80
iulianpro <iulian.prodea@gmail.com>
2020-05-22 01:31:51 +0100

    Created GiftCategory db model

--- gifts/models.py
+++ gifts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class GiftCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
