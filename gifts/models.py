84970c6011599515f570b6b636cf10b559be1d22
iulianpro <iulian.prodea@gmail.com>
2020-05-22 01:42:40 +0100

    Created Gift db model

--- gifts/models.py
+++ gifts/models.py
@@ -7,3 +7,22 @@ class GiftCategory(models.Model):
 
     def __str__(self):
         return self.name + ' (id: ' + self.id + ')'
+
+
+class Gift(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    currency = models.CharField(max_length=3, default="GBP")
+    image = models.ImageField(upload_to="images")
+    category = models.ForeignKey(
+        "gifts.GiftCategory",
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name + ' (' + self.price + ' ' + self.currency + ' - ID: ' + self.id + ')'
