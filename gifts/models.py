309809e92138f1fd1bc436d01bd3ee370fe79a7e
iulianpro <iulian.prodea@gmail.com>
2020-05-22 01:32:18 +0100

    Updated GiftCategory db model

--- gifts/models.py
+++ gifts/models.py
@@ -6,4 +6,4 @@ class GiftCategory(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name
+        return self.name + ' (id: ' + self.id + ')'
