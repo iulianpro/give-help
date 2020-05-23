d43f39acc806fcb2f2309c2bd3ac136cc5575573
iulianpro <iulian.prodea@gmail.com>
2020-05-23 01:56:31 +0100

    Added 'add to cart' view and url to cart app

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import view_cart
+from .views import view_cart, add_to_cart
 
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
+    url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 
 def view_cart(request):
     return render(request, "cart.html")
+
+
+def add_to_cart(request, id):
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity
+    else:
+        cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
