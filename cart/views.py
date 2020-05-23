d382fa6944eee18e9a81186ee1d743da5b465e9a
iulianpro <iulian.prodea@gmail.com>
2020-05-23 21:55:40 +0100

    Changed redirect page after gift is added to cart

--- cart/views.py
+++ cart/views.py
@@ -15,7 +15,7 @@ def add_to_cart(request, id):
         cart[id] = cart.get(id, quantity)
 
     request.session['cart'] = cart
-    return redirect(reverse('index'))
+    return redirect(reverse('gifts'))
 
 
 def adjust_cart(request, id):
