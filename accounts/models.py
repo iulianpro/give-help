946e9ff5a4b06a02c9157ddec1eb6fa7f4fb7f70
iulianpro <iulian.prodea@gmail.com>
2020-05-26 23:48:08 +0100

    Changed accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import User
 
 class Donor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    phone_number = models.IntegerField(default=0, blank=True)
-    postcode = models.CharField(max_length=10, blank=True)
-    town_city = models.CharField(max_length=20, blank=True)
-    street_address1 = models.CharField(max_length=50, blank=True)
-    street_address2 = models.CharField(max_length=50, null=True, blank=True)
-    county = models.CharField(max_length=20, blank=True)
+    phone_number = models.CharField(max_length=20, blank=False)
+    postcode = models.CharField(max_length=10, blank=False)
+    town_city = models.CharField(max_length=20, blank=False)
+    street_address1 = models.CharField(max_length=50, blank=False)
+    street_address2 = models.CharField(max_length=50, blank=False, default='')
+    county = models.CharField(max_length=20, blank=False)
 
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name + ' (' + self.user.profile.town_city + ')'
