0378909298e97c3eb80422ba475d8e7a9638c477
iulianpro <iulian.prodea@gmail.com>
2020-05-27 01:22:48 +0100

    Added checkout urls to project urls

--- give_help/urls.py
+++ give_help/urls.py
@@ -19,6 +19,7 @@ from home.views import index
 from accounts import urls as urls_accounts
 from gifts import urls as urls_gifts
 from cart import urls as urls_cart
+from checkout import urls as urls_checkout
 from gifts.views import all_gifts
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -30,5 +31,6 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^shop/gifts', include(urls_gifts)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
