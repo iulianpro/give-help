99efbe677a84e900922e301cca586b23851c3b63
iulianpro <iulian.prodea@gmail.com>
2020-05-27 01:19:29 +0100

    Changed checkout order model

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from gifts.models import Gift
 
-# Create your models here.
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=50, blank=False, default='')
+    phone_number = models.CharField(max_length=20, blank=False, default='')
+    country = models.CharField(max_length=40, blank=False, default='')
+    postcode = models.CharField(max_length=20, blank=True, default='')
+    town_or_city = models.CharField(max_length=40, blank=False, default='')
+    street_address1 = models.CharField(max_length=40, blank=False, default='')
+    street_address2 = models.CharField(max_length=40, blank=False, default='')
+    county = models.CharField(max_length=40, blank=False, default='')
+    date = models.DateField()
+
+    def __str__(self):
+        return "Order id {0} | {1} | {2}".format(self.id, self.full_name, self.date)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False)
+    gift = models.ForeignKey(Gift, null=False)
+    quantity = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return "{0} | {1} | {2}".format(
+            self.quantity, self.gift.name, self.gift.price)
