f9d1f9fdd87dab053edf12a5e8ab030d32f2483d
iulianpro <iulian.prodea@gmail.com>
2020-05-29 07:58:56 +0100

    Added subscribe form to render in gifts view

--- gifts/views.py
+++ gifts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Gift
+from subscribe.forms import EmailSignupForm
 
 
 def all_gifts(request):
     gifts = Gift.objects.all()
-    return render(request, 'gifts.html', {'gifts': gifts})
+    subscribe_form = EmailSignupForm()
+    return render(request, 'gifts.html', {'gifts': gifts, 'subscribe_form': subscribe_form})
