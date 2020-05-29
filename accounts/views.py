d0cf6dbbefa1d51e674f891bfdf4c9cdeb5c1437
iulianpro <iulian.prodea@gmail.com>
2020-05-29 07:46:53 +0100

    Added subsribe form to account views

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from accounts.models import Donor
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from subscribe.forms import EmailSignupForm
 
 # Create your views here.
 
@@ -21,6 +22,8 @@ def logout(request):
 def login(request):
     if request.user.is_authenticated:
         return redirect(reverse('index'))
+
+    subscribe_form = EmailSignupForm()
 
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
@@ -38,13 +41,15 @@ def login(request):
                 messages.error(request, "Username or password is incorrect")
     else:
         login_form = UserLoginForm()
-    return render(request, 'login.html', {'login_form': login_form})
+    return render(request, 'login.html', {'login_form': login_form, 'subscribe_form': subscribe_form})
 
 
 def registration(request):
     if request.user.is_authenticated:
         messages.error(request, "You are already logged in")
         return redirect(reverse('index'))
+
+    subscribe_form = EmailSignupForm()
 
     if request.method == "POST":
         registration_form = UserRegistrationForm(request.POST)
@@ -65,13 +70,14 @@ def registration(request):
     else:
         registration_form = UserRegistrationForm()
 
-    return render(request, 'registration.html', {"registration_form": registration_form})
+    return render(request, 'registration.html', {"registration_form": registration_form, 'subscribe_form': subscribe_form})
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Donor.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
@@ -80,13 +86,18 @@ def save_user_profile(sender, instance, **kwargs):
 
 def view_profile(request):
     donor = Donor.objects.get(user_id=request.user.id)
-    return render(request, 'profile.html', {"donor": donor})
+    subscribe_form = EmailSignupForm()
+    return render(request, 'profile.html', {"donor": donor, 'subscribe_form': subscribe_form})
 
 
 def edit_profile(request):
+    subscribe_form = EmailSignupForm()
+
     if request.method == "POST":
-        edit_profile_form = EditProfileForm(request.POST, instance=request.user)
-        edit_donor_form = DonorForm(request.POST, instance=request.user.profile)
+        edit_profile_form = EditProfileForm(
+            request.POST, instance=request.user)
+        edit_donor_form = DonorForm(
+            request.POST, instance=request.user.profile)
 
         if edit_profile_form.is_valid() and edit_donor_form.is_valid():
             edit_profile_form.save()
@@ -98,4 +109,4 @@ def edit_profile(request):
     else:
         edit_profile_form = EditProfileForm(instance=request.user)
         edit_donor_form = DonorForm(instance=request.user.profile)
-        return render(request, 'edit-profile.html', {"edit_profile_form": edit_profile_form, 'edit_donor_form': edit_donor_form})
+        return render(request, 'edit-profile.html', {"edit_profile_form": edit_profile_form, 'edit_donor_form': edit_donor_form, 'subscribe_form': subscribe_form})
