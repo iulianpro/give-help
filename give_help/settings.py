eeda444829ac1e90d1fe47a5c4c7fa56f34ea139
iulianpro <iulian.prodea@gmail.com>
2020-05-31 04:53:56 +0100

    Set DEBUG according to server where project is running

--- give_help/settings.py
+++ give_help/settings.py
@@ -24,7 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if 'USE_DEBUG' in os.environ:
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['iulianpro-give-help.herokuapp.com', '127.0.0.1']
 
