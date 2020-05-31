d737c13ca66422b1a18122f79c625bddadb3c094
iulianpro <iulian.prodea@gmail.com>
2020-05-31 01:23:04 +0100

    Added setting to deploy to Heroku - part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn give_help.wsgi:application
--- give_help/settings.py
+++ give_help/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iulianpro-give-help.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -82,17 +82,16 @@ WSGI_APPLICATION = 'give_help.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#        }
-#    }
-
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.getenv('DATABASE_URL'))
-
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
 
 # Password validation
